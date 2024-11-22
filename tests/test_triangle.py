2f56f7bcac3b7f03828b2f2635a7805b7602ace5
makkssik <maksimaristov2006@mail.ru>
2024-11-22 18:55:23 +0300

    Fixing functions and adding unit tests

---
+++ tests/test_triangle.py
@@ -0,0 +1,40 @@
+import unittest
+from triangle import area, perimeter
+
+class TestAreaTriangle(unittest.TestCase):
+
+    def test_area(self):
+        a, b, c = 3, 4, 5  # Прямоугольный треугольник
+        expected_area = 6.0  # Площадь = 1/2 * основание * высота
+        result = area(a, b, c)
+        self.assertAlmostEqual(result, expected_area)
+
+    def test_area_invalid_triangle(self):
+        invalid_triangles = [
+            (1, 1, 3),  # Неравенство треугольника не выполняется
+            (1, 2, 3),
+            (0, 1, 1),  # Стороны не могут быть нулевыми или отрицательными
+            (-1, 1, 1)
+        ]
+        for sides in invalid_triangles:
+            with self.subTest(sides=sides):
+                with self.assertRaises(ValueError):
+                    area(*sides)
+
+class TestPerimeterTriangle(unittest.TestCase):
+
+    def test_perimeter(self):
+        a, b, c = 3, 4, 5
+        expected_perimeter = 12
+        result = perimeter(a, b, c)
+        self.assertEqual(result, expected_perimeter)
+
+    def test_perimeter_invalid_side(self):
+        invalid_sides = [(0, 1, 1), (-1, 1, 1)]
+        for sides in invalid_sides:
+            with self.subTest(sides=sides):
+                with self.assertRaises(ValueError):
+                    perimeter(*sides)
+
+if __name__ == '__main__':
+    unittest.main()
